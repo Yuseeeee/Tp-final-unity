7445d66edf7a674240911f63841bcce84a75df42
Yuseeeee <49379138@est.ort.edu.ar>
2025-11-25 12:06:49 -0300

    se va falta luz y addforce mejor

---
+++ Assets/AgarrarPelota.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarrarPelota : MonoBehaviour
+{
+    public float pickupDistance = 3f;
+    public LayerMask ballLayer;
+    public TirarPelota TirarPelota;           
+    public Transform PuntoDeAgarre;           
+
+    void Update()
+    {
+        TryPickupBall();
+        Debug.DrawRay(transform.position, transform.forward * pickupDistance, Color.red);
+
+    }
+
+    void TryPickupBall()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickupDistance, ballLayer))
+        {
+            Rigidbody ball = hit.collider.GetComponent<Rigidbody>();
+
+            if (ball != null && TirarPelota.currentBall == null)
+            {
+                Pickup(ball);
+            }
+        }
+    }
+
+    void Pickup(Rigidbody ball)
+    {
+        TirarPelota.currentBall = ball;
+        ball.isKinematic = true;                        
+        ball.transform.position = PuntoDeAgarre.position;   
+        ball.transform.rotation = PuntoDeAgarre.rotation;
+        ball.transform.parent = PuntoDeAgarre;              
+    }
+}
---
+++ Assets/TirarPelota.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirarPelota : MonoBehaviour
+{
+    public Rigidbody currentBall; 
+
+    public Rigidbody ballPrefab;
+    public Transform shootPoint;
+
+    public float force = 500f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        if (currentBall != null)
+        {
+            currentBall.transform.parent = null; 
+            currentBall.isKinematic = false;     
+            currentBall.AddForce(shootPoint.forward * force);
+            currentBall = null;
+        }
+        else
+        {
+            Rigidbody newBall = Instantiate(ballPrefab, shootPoint.position, shootPoint.rotation);
+            newBall.AddForce(shootPoint.forward * force);
+        }
+    }
+}
+
